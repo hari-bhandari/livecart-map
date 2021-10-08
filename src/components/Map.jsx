649fab37a760128f23a8394d2866c9034ff3da20
hari-bhandari <2012bhandari.ha@gmail.com>
2021-10-08 10:29:45 +0100

    refetching whenever user drags more than 1000 metres

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,41 +1,49 @@
 import React, {useEffect, useState} from 'react';
 import GoogleMapReact from 'google-map-react';
-import Logo from "./MapCustomLogo/Logo";
 import useFetch from "./useFetch";
 import ReactLoading from 'react-loading';
 import {measure, sizes} from "./utils";
+import Logo from "./MapCustomLogo/Logo";
 
 const MyLocation = ({zoom}) => (
     <img src="https://res.cloudinary.com/wisecart/image/upload/v1633590498/location_js4dam.png" alt="My Location" style={{width:sizes[zoom]}} />
 )
-
+const zoomLevelIntoMetres = (zoomLevel) => {
+  return (40000/(2**zoomLevel))*2000
+}
 
 const Map = ({location,setLocation}) => {
-    const [range, setRange] = useState(24932)//range
     const [zoom, setZoom] = useState(14) //zoom
+
     // lat: 40.807537,
     //     lng: -73.962570
-    const {loading, error, data} = useFetch(location.lat, location.lng, range)
+    const {loading, error, data} = useFetch(location.lat, location.lng, zoomLevelIntoMetres(zoom))
     const OnDragEnd = (map) => {
         const distance=measure(location.lat,location.lng,map.center.lat(),map.center.lng())
         console.log(distance)
-        if(distance>4000){
+        if(distance>1000){
             setLocation({lat:map.center.lat(),lng:map.center.lng()})
         }
     }
-    useEffect(() => {
-        //to do list
-        //make the range bigger if data is not found
-        //zoom out map too.
-        // if (data?.length === 0) { //only updates range when the array is empty
-        //     if (range < range * 2) {
-        //         setRange(range * 2) //doubling the range //distance
-        //         setZoom(zoom - 2)
-        //     }
-        // }
-    }, [data])
-    const handleApiLoaded = (map, maps) => {
-        console.log({map,maps})
+    // useEffect(() => {
+    //     //to do list
+    //     //make the range bigger if data is not found
+    //     //zoom out map too.
+    //     // if (data?.length === 0) { //only updates range when the array is empty
+    //     //     if (range < range * 2) {
+    //     //         setRange(range * 2) //doubling the range //distance
+    //     //         setZoom(zoom - 2)
+    //     //     }
+    //     // }
+    // }, [data])
+
+    const handleApiLoaded = (map, maps,places) => {
+        const bounds =new maps.LatLngBounds()
+        console.log(bounds)
+        // Fit map to bounds
+        // Bind the resize listener
+        // bindResizeListener(map, maps, bounds);
+
     };
 
     return (
@@ -48,9 +56,6 @@ const Map = ({location,setLocation}) => {
                     zoom={zoom}
                     onDragEnd={(map) => OnDragEnd(map)}
                     yesIWantToUseGoogleMapApiInternals
-                    onChildClick={(id) => {
-                        console.log({id})
-                    }}
                     onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
                     onZoomAnimationEnd={(data)=>{
                         setZoom(data)
